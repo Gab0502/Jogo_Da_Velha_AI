f74871656a25beb35d09423f13ae5efd618daa96
Gabriel Marquez <104799843+Gab0502@users.noreply.github.com>
2023-08-13 16:01:46 -0300

    updated defense
    defense done in more directions, fixed out of range problems

--- main.py
+++ main.py
@@ -39,31 +39,44 @@ while True:
         prioridade = []
         # quinas e centro
         if rodada == 1:
-            if mapa[0][0] != "O":
+            if mapa[0][0] == ",":
                 prioridade.append((0, 0))
-            if mapa[0][2] != "0":
+            if mapa[0][2] == ",":
                 prioridade.append((0, 2))
-            if mapa[1][1] != "0":
+            if mapa[1][1] == ",":
                 prioridade.append((1, 1))
-            if mapa[2][0] != "0":
+            if mapa[2][0] == ",":
                 prioridade.append((2, 0))
-            if mapa[2][2] != "0":
+            if mapa[2][2] == ",":
                 prioridade.append((2, 2))
 
             escolha_aleatoria = random.choice(prioridade)
             mapa[escolha_aleatoria[0]][escolha_aleatoria[1]] = "X"
 
         else:
+            # defesa
+            if mapa[0][0]=="O" and mapa[0][2]=="O":
+                mapa[0][1]="X"
+            elif mapa[1][0]=="O" and mapa[1][2]=="O":
+                mapa[1][1]="X"
+            elif mapa[2][0]=="O" and mapa[2][2]=="O":
+                mapa[2][1]="X"
+            elif mapa[0][2]=="O" and mapa[2][0]=="O":
+                mapa[1][1]="X"
+            elif mapa[0][2]=="O" and mapa[1][1]=="O":
+                mapa[2][0]="X"
             for i in range(len(mapa)):
                 for j in range(len(mapa)):
-                 #defesa
-                    if mapa[i][j]=="O" and mapa[i][j+1]=="O":
-                        mapa[i][j+2]="X"
-                    elif mapa[i][j]=="O" and mapa[i+1][j]=="O":
-                        mapa[i+2][j]="X"
-                    elif mapa[i][i]=="O" and mapa[i+1][i+1]=="O":
-                        mapa[i+2][i+2]="X"
-
+                    if mapa[i][j] == "O" and mapa[i][(j + 1) % 3] == "O" and mapa[i][(j + 2) % 3] == ",":
+                        mapa[i][(j + 2) % 3] = "X"
+                    elif mapa[j][i] == "O" and mapa[(j + 1) % 3][i] == "O" and mapa[(j + 2) % 3][i] == ",":
+                        mapa[(j + 2) % 3][i] = "X"
+                    elif mapa[i][i] == "O" and mapa[(i + 1) % 3][(i + 1) % 3] == "O" and mapa[(i + 2) % 3][
+                        (i + 2) % 3] == ",":
+                        mapa[(i + 2) % 3][(i + 2) % 3] = "X"
+            else:
+                #jogada ofensiva bot
+                
         print("Jogada da velha")
 
     printa_matriz(mapa)
